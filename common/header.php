c2bb8d186873537f3b3963471cc92320c5a7c463
Xavier Beurois <xbeurois@djazz-univ.net>
2015-05-05 16:09:49 +0200

    Fix minor bug in the page header

--- common/header.php
+++ common/header.php
@@ -22,10 +22,10 @@
 				<div class="masthead">
 					<ul class="nav nav-pills pull-right">
 						<li class="nav-home<?php print((!isset($_GET['p']) || $_GET['p'] == 'home.php')?' active':''); ?>"><a href="home.php">Home</a></li>
-						<li class="nav-new<?php print(($_GET['p'] == 'new-cronjob.php')?' active':''); ?>"><a href="new-cronjob.php">New Cronjob</a></li>
-						<li class="nav-active<?php print(($_GET['p'] == 'active-cronjobs.php')?' active':''); ?>"><a href="active-cronjobs.php">Active Cronjobs</a></li>
-						<li class="nav-all<?php print(($_GET['p'] == 'all-cronjobs.php')?' active':''); ?>"><a href="all-cronjobs.php">All Cronjobs</a></li>
-						<li class="nav-admin<?php print(($_GET['p'] == 'admin.php')?' active':''); ?>"><a href="admin.php">Admin</a></li>
+						<li class="nav-new<?php print((isset($_GET['p']) && strlen($_GET['p']) > 0 && $_GET['p'] == 'new-cronjob.php')?' active':''); ?>"><a href="new-cronjob.php">New Cronjob</a></li>
+						<li class="nav-active<?php print((isset($_GET['p']) && strlen($_GET['p']) > 0 && $_GET['p'] == 'active-cronjobs.php')?' active':''); ?>"><a href="active-cronjobs.php">Active Cronjobs</a></li>
+						<li class="nav-all<?php print((isset($_GET['p']) && strlen($_GET['p']) > 0 && $_GET['p'] == 'all-cronjobs.php')?' active':''); ?>"><a href="all-cronjobs.php">All Cronjobs</a></li>
+						<li class="nav-admin<?php print((isset($_GET['p']) && strlen($_GET['p']) > 0 && $_GET['p'] == 'admin.php')?' active':''); ?>"><a href="admin.php">Admin</a></li>
 					</ul>
 					<h1><a href="home.php">CronWeb <span class="muted">by SGC-Univ.Net</span></a></h1>
 				</div>
